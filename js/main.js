208419d17b2373676bca4ec5e50433e6a038301e
AX5-Code <ahmed-attia@my.com>
2023-04-09 22:58:46 +0200

    com02

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 let link = document.querySelector(".linkes");
 let ul = document.querySelector("#ul");
 let minupanel = document.querySelector("#minupanel");
-function minu() {
+function mine() {
 	if (ul.style.display == "none") {
 		ul.style.display = "block";
 	} else {
 		ul.style.display = "none";
 	}
 }
-function valed() {
+function valid() {
 	ul.style.display = "none";
 }
-link.addEventListener("click", minu);
-minupanel.addEventListener("mouseenter", valed);
+link.addEventListener("click", mine);
+minupanel.addEventListener("mouseenter", valid);
