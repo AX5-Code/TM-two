e24e2223ef24af80105cb882aaa4e90a3242158a
AX5-Code <ahmed-attia@my.com>
2023-04-13 12:41:55 +0200

    final headers editing

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-let link = document.querySelector(".linkes");
-let ul = document.querySelector("#ul");
-let minupanel = document.querySelector("#minupanel");
-function mine() {
-	if (ul.style.display == "none") {
-		ul.style.display = "block";
-	} else {
-		ul.style.display = "none";
-	}
-}
-function valid() {
-	ul.style.display = "none";
-}
-link.addEventListener("click", mine);
-minupanel.addEventListener("mouseenter", valid);
+// let link = document.querySelector(".linkes");
+// let ul = document.querySelector("#ul");
+// let minupanel = document.querySelector("#minupanel");
+// function mine() {
+// 	if (ul.style.display == "none") {
+// 		ul.style.display = "block";
+// 	} else {
+// 		ul.style.display = "none";
+// 	}
+// }
+// function valid() {
+// 	ul.style.display = "none";
+// }
+// link.addEventListener("click", mine);
+// minupanel.addEventListener("mouseenter", valid);
