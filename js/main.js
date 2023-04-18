0a3194c6d09b2f42764f98855372f0b8780a0f4e
AX5-Code <ahmed-attia@my.com>
2023-04-18 10:34:49 +0200

    potfolio

--- js/main.js
+++ js/main.js
@@ -1,15 +1,11 @@
-// let link = document.querySelector(".linkes");
-// let ul = document.querySelector("#ul");
-// let minupanel = document.querySelector("#minupanel");
-// function mine() {
-// 	if (ul.style.display == "none") {
-// 		ul.style.display = "block";
-// 	} else {
-// 		ul.style.display = "none";
-// 	}
-// }
-// function valid() {
-// 	ul.style.display = "none";
-// }
-// link.addEventListener("click", mine);
-// minupanel.addEventListener("mouseenter", valid);
+let fluty = document.querySelector(".flout");
+window.onscroll = () => {
+	if (scrollY >= 350) {
+		fluty.style.display = "block";
+  } else {
+    fluty.style.display = "none";
+  }
+};
+fluty.onclick = () => {
+	window.scrollTo({ top: "0", behavior: "smooth" });
+};
